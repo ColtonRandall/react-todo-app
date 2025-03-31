498655164361982d41a0771ae534f683f265c8dd
Colton Randall <coltonrandall.nz@gmail.com>
2025-03-31 18:42:26 +1300

    styling and base structure

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add Todo</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Todo item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
+}
